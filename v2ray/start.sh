0e1254e8a636a171bfe1feb1719a1ab1f429df7f
猫先生 <fang.dajiang@gmail.com>
2024-11-15 19:21:17 +0800

    Update start.sh

--- v2ray/start.sh
+++ v2ray/start.sh
@@ -27,5 +27,5 @@ sed -i "s/\$PATH_RAY/\\${PATH_RAY}/" /etc/v2ray/v2ray.json
 # sh -c "watch -n 60 /monitor.sh">&1 &
 
 # 主进程
-env v2ray.vmess.aead.forced = true
-v2ray --config=/etc/v2ray/v2ray.json
+env "v2ray.vmess.aead.forced" = true
+v2ray run --config=/etc/v2ray/v2ray.json
